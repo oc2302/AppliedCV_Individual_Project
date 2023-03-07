9c172a340148eaa8df5b3d9b6b1b9cd6f0cb34ab
Olivia <91628093+oc2302@users.noreply.github.com>
2023-03-06 22:21:28 -0500

    Create otsu_thresholding.py

---
+++ otsu_thresholding.py
@@ -0,0 +1,34 @@
+#Otsu_thresholding method
+import cv2
+import histomicstk as htk
+
+import numpy as np
+import scipy as sp
+
+import skimage.io
+import skimage.measure
+import skimage.color
+
+#Example image-----------------------------------------
+input_image_file = ('https://data.kitware.com/api/v1/file/576ad39b8d777f1ecd6702f2/download')  
+
+im_input = skimage.io.imread(input_image_file)[:, :, :3]
+
+#plt.imshow(im_input)
+#_ = plt.title('Input Image', fontsize=16)
+
+
+
+
+
+
+# Load the image
+img = cv2.imread('image.jpg', 0) # 0 flag to read grayscale image
+
+# Apply Otsu thresholding
+ret, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# Display the thresholded image
+cv2.imshow('Otsu Threshold', thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

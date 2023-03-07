3880d4eefd6dc7cd6ed259b58fd1295c44484d3b
Olivia <91628093+oc2302@users.noreply.github.com>
2023-03-06 22:48:42 -0500

    Update chan-vese.py

--- chan-vese.py
+++ chan-vese.py
@@ -1,4 +1,4 @@
-#K-means clustering implementation
+#Chan-vese method implementation
 
 # Imports ------------------------------------------
 import histomicstk as htk
@@ -37,14 +37,12 @@ mean_ref, std_ref = htk.preprocessing.color_conversion.lab_mean_std(im_reference
 im_nmzd = htk.preprocessing.color_normalization.reinhard(im_input, mean_ref, std_ref)
 
 
-img = plt.imread('image.jpg', 0)
-
 # Perform Chan-Vese clustering
-cv = chan_vese(img, mu=0.25, lambda1=1, lambda2=1, tol=1e-3, max_iter=200, dt=0.5)
+cv = chan_vese(img_nmzd, mu=0.25, lambda1=1, lambda2=1, tol=1e-3, max_iter=200, dt=0.5)
 
 # Display the segmented image
 fig, ax = plt.subplots(figsize=(6, 6))
-ax.imshow(img, cmap='gray')
+ax.imshow(img_nmzd, cmap='gray')
 ax.contour(cv, [0.5], colors='r', linewidths=2)
 ax.axis('off')
 plt.show()
